ba43f74a750d1499da8ea27db7b46df47470cb6d
Harry Jiang <jxx1998@gmail.com>
2020-02-26 11:59:41 -0800

    Add ShapeBuilder and Shape methods

--- BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/Shape.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/Shape.java
@@ -1,12 +1,13 @@
 package edu.stanford.cs108.bunnyworld;
 
+import android.graphics.Canvas;
 import android.graphics.RectF;
 
 public class Shape {
 
     String name;
     RectF coordinates;
-    boolean inPossessionsArea;
+    boolean inInventory;
     String pageName; // String is null if item is in Possessions Area
     String imageName; // Name of the image this Shape can draw
     String text; // Some text that this Shape can draw
@@ -16,11 +17,100 @@ public class Shape {
     String[] scripts;
     boolean selected;
 
+    // Do not call this Shape constructor directly; use ShapeBuilder to construct a new Shape
+    // See ShapeBuilder documentation for creating a new Shape
+    public Shape(String name, RectF coordinates, boolean inInventory, String pageName,
+                 String imageName, String text, int textSize, boolean hidden, boolean movable,
+                 String[] scripts, boolean selected) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.inInventory = inInventory;
+        this.pageName = pageName;
+        this.imageName = imageName;
+        this.text = text;
+        this.textSize = textSize;
+        this.hidden = hidden;
+        this.movable = movable;
+        this.scripts = scripts;
+        this.selected = selected;
+    }
+
+    // Getters
+    public RectF getRectF() { return coordinates; }
     public float getLeft() { return coordinates.left; }
     public float getRight() { return coordinates.right; }
     public float getBottom() { return coordinates.bottom; }
     public float getTop() { return coordinates.top; }
     public float getWidth() { return coordinates.width(); }
     public float getHeight() { return coordinates.height(); }
-    
+    public String getName() { return this.name; }
+    public boolean isInInventory() { return inInventory; }
+    public String getPageName() { return pageName; }
+    public String getImageName() { return imageName; }
+    public String getText() { return text; }
+    public int getTextSize() { return textSize; }
+    public boolean isHidden() { return hidden; }
+    public boolean isMovable() { return movable; }
+    public String[] getScripts() { return scripts; }
+    public boolean isSelected() { return selected; }
+
+    // Below are some Setters
+
+    // Set Coordinates directly with a RectF
+    public void setCoordinates(RectF coordinates) {
+        coordinates.sort();
+        this.coordinates = coordinates;
+    }
+
+    // Set Coordinates with (left, top, right, bottom)
+    public void setCoordinates(float left, float top, float right, float bottom) {
+        RectF coordinates = new RectF(left, top, right, bottom);
+        setCoordinates(coordinates);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Please remember to set Coordinates after calling this
+    public void moveToInventory(boolean inInventory) {
+        this.inInventory = true;
+        this.pageName = null;
+    }
+
+    // Please remember to set Coordinates after calling this
+    public void moveToPage(String pageName) {
+        this.pageName = pageName;
+        this.inInventory = false;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public void setMovable(boolean movable) {
+        this.movable = movable;
+    }
+
+    public void setScripts(String[] scripts) {
+        this.scripts = scripts;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    // Other methods
+    public void draw(Canvas canvas) {
+
+    }
+
+    // Returns whether a given (x, y) is located within the Shape
+    public boolean contains(float x, float y) {
+        return coordinates.contains(x, y);
+    }
 }
---
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/ShapeBuilder.java
@@ -0,0 +1,106 @@
+package edu.stanford.cs108.bunnyworld;
+
+import android.graphics.RectF;
+
+/**
+ * The ShapeBuilder is a helper class that helps with constructing a new Shape.
+ *
+ * Example usage: Shape shape = ShapeBuilder()
+ *                              .name("my_shape")
+ *                              .coordinates(left, top, right, bottom)
+ *                              .pageName("page_1")
+ *                              .imageName("bunny")
+ *                              .text("Hello bunny!", 20)
+ *                              .hidden(true)
+ *                              .movable(true)
+ *                              .scripts(my_scripts)
+ *                              .selected(true)
+ *                              .buildShape();
+ *
+ * Required fields that must be specified: .name() and .coordinates()
+ * If .name() or .coordinates() not specified, buildShape() returns null
+ * If no pageName is specified, by default the Shape is in the Inventory area.
+ * By default, the Shape is not hidden, and is movable.
+ * There are two ways to specify coordinates: either pass in a RectF, or (left, top, right, bottom) in floats.
+ */
+
+public class ShapeBuilder {
+
+    String name;
+    RectF coordinates;
+    boolean inInventory = true;
+    String pageName; // String is null if item is in Possessions Area
+    String imageName; // Name of the image this Shape can draw
+    String text; // Some text that this Shape can draw
+    int textSize; // The size of the text in case Shape needs to draw the text
+    boolean hidden = false; // Whether this shape should be drawn out/clickable in Play time
+    boolean movable = true; // Whether this shape can be dragged around during Play time
+    String[] scripts;
+    boolean selected = false;
+
+    public ShapeBuilder() {
+        scripts = new String[0];
+    }
+
+    public Shape buildShape() {
+        if (name == null || coordinates == null) {
+            return null;
+        } else {
+            return new Shape(name, coordinates, inInventory, pageName, imageName, text, textSize, hidden, movable, scripts, selected);
+        }
+    }
+
+    public ShapeBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ShapeBuilder coordinates(RectF coordinates) {
+        coordinates.sort();
+        this.coordinates = coordinates;
+        return this;
+    }
+
+    public ShapeBuilder coordinates(float left, float top, float right, float bottom) {
+        this.coordinates = new RectF(left, top, right, bottom);
+        return coordinates(this.coordinates);
+    }
+
+    public ShapeBuilder pageName(String pageName) {
+        this.inInventory = false;
+        this.pageName = pageName;
+        return this;
+    }
+
+    public ShapeBuilder imageName(String imageName) {
+        this.imageName = imageName;
+        return this;
+    }
+
+    public ShapeBuilder text(String text, int textSize) {
+        this.text = text;
+        this.textSize = textSize;
+        return this;
+    }
+
+    public ShapeBuilder hidden(boolean hidden) {
+        this.hidden = hidden;
+        return this;
+    }
+
+    public ShapeBuilder movable(boolean movable) {
+        this.movable = movable;
+        return this;
+    }
+
+    public ShapeBuilder scripts(String[] scripts) {
+        this.scripts = scripts;
+        return this;
+    }
+
+    public ShapeBuilder selected(boolean selected) {
+        this.selected = selected;
+        return this;
+    }
+
+}

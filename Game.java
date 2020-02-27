d49d8fef97af39b8c12d74f16465e08cb3b86d69
blakevill <41968049+blakevill@users.noreply.github.com>
2020-02-27 12:43:00 -0800

    Game Database setup
    Setup works, but need also to retrieve from database and make sure that data keeps its integrity

---
+++ Game.java
@@ -0,0 +1,16 @@
+package edu.stanford.cs108.bunnyworld;
+
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Page> pages;
+
+    public Game() {
+        pages = null;
+    }
+
+    public Game(ArrayList<Page> pageList) {
+        pages = pageList;
+    }
+
+}

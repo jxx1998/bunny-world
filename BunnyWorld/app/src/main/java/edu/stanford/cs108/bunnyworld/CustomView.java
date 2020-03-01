c25255f0e75ddcd4d534d9584c679019392aa507
Costa <costa99@stanford.edu>
2020-03-01 15:35:22 -0800

    basic page,shape,customview functionality working

--- BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/CustomView.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/CustomView.java
@@ -14,6 +14,7 @@ public class CustomView extends View {
     Paint blueFillPaint;
     int viewWidth;
     int viewHeight;
+    Page currPage;
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -27,13 +28,27 @@ public class CustomView extends View {
         redOutlinePaint.setStrokeWidth(5.0f);
         blueFillPaint = new Paint();
         blueFillPaint.setColor(Color.BLUE);
+
+        //Creating a new page and adding shapes to it.
+        currPage = new Page("initialExamplePage");
+        Shape shape1 = new ShapeBuilder().name("shape1").coordinates(80f,250f,400f,450f).imageName("duck").buildShape();
+        Shape shape2 = new ShapeBuilder().name("shape2").coordinates(1000f,250f,1300f,350f).imageName("carrot").buildShape();
+        Shape shape3 = new ShapeBuilder().name("shape3").coordinates(500f,50f,1000f,550f).imageName("death").buildShape();
+        currPage.addShape(shape1);
+        currPage.addShape(shape2);
+        currPage.addShape(shape3);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawLine(0.0f,0.0f,viewWidth,viewHeight,redOutlinePaint);
-        canvas.drawRect(50.0f,50.0f,150.0f,150.0f,blueFillPaint);
+//        canvas.drawLine(0.0f,0.0f,viewWidth,viewHeight,redOutlinePaint);
+//        canvas.drawRect(50.0f,50.0f,150.0f,150.0f,blueFillPaint);
+        currPage.draw(canvas);
+
+
+
     }
 
     @Override

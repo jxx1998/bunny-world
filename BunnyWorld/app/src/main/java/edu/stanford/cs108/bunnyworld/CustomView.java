335a8d16a6af683429f2bd7d27a546623e642797
Costa <costa99@stanford.edu>
2020-03-01 15:49:41 -0800

    trying mobile draw on custom view

--- BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/CustomView.java
+++ BunnyWorld/app/src/main/java/edu/stanford/cs108/bunnyworld/CustomView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -15,10 +16,15 @@ public class CustomView extends View {
     int viewWidth;
     int viewHeight;
     Page currPage;
+    float x1, x2, y1, y2, left, right, top, bottom;
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
+    }
+
+    public void setCurrPage(Page currPage) {
+        this.currPage = currPage;
     }
 
     private void init(){
@@ -45,6 +51,9 @@ public class CustomView extends View {
         super.onDraw(canvas);
 //        canvas.drawLine(0.0f,0.0f,viewWidth,viewHeight,redOutlinePaint);
 //        canvas.drawRect(50.0f,50.0f,150.0f,150.0f,blueFillPaint);
+        System.out.println("left, top, right, bottom:" + left + ", " + top + ", " + right + ", " + bottom);
+        Shape newShape = new ShapeBuilder().name("shape1").coordinates(left,top,right,bottom).imageName("carrot2").buildShape();
+        currPage.addShape(newShape);
         currPage.draw(canvas);
 
 
@@ -56,5 +65,43 @@ public class CustomView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         viewWidth = w;
         viewHeight = h;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            //When you first click, that's the initial x,y coord pair
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+
+                //At this point, we've released the click and have the two points
+                //of the rectangle
+
+                if (x2 > x1) {
+                    right = x2;
+                    left = x1;
+                } else {
+                    right = x1;
+                    left = x2;
+                }
+
+                if (y2 > y1) {
+                    bottom = y2;
+                    top = y1;
+                } else {
+                    bottom = y1;
+                    top = y2;
+                }
+
+        }
+
+        return true;
     }
 }
